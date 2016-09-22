fd979c3a60db53821d4b1496e6da21a0a999cbc8
Igor Khudik <js.web23@gmail.com>
2016-09-22 23:46:34 +0300

    Make authentication

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import { connect } from 'react-redux'
+import * as actions from '../actions'
 
-export default class Header extends Component {
+class Header extends Component {
     authButton() {
-        return <button>Sign In</button>;
+        if (this.props.authenticated) {
+            return <button onClick={() => this.props.authenticate(false)}>Sign out</button>
+;        }
+        return <button onClick={() => this.props.authenticate(true)}>Sign In</button>;
     }
     render() {
         return (
@@ -23,3 +28,9 @@ export default class Header extends Component {
         )
     }
 }
+
+function mapStateToProps(state) {
+    return { authenticated: state.authenticated }
+}
+
+export default connect(mapStateToProps, actions)(Header);
