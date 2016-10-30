92bc11523d16f2d7ba48a30b5e94af3ac9dd923f
Igor Khudik <js.web23@gmail.com>
2016-10-30 11:55:09 +0200

    Handle success signin

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,23 @@
 import axios from 'axios';
+import {browserHistory} from 'react-router';
+import {AUTH_USER} from './types';
 
 const ROOT_URL = 'http://localhost:3090';
 
 export function signinUser({email, password}) {
     return function(dispatch, getState) {
         // Submit data to the server
-        axios.post(`${ROOT_URL}/signin`, {email, password});
+        axios.post(`${ROOT_URL}/signin`, {email, password})
+            .then(response => {
+                // If success - update state; save jwt; redirect to /feature
+                dispatch({type: AUTH_USER});
+                localStorage.setItem('token', response.data.token);
+                browserHistory.push('/feature');
+            })
+            .catch(() => {
+                // If error - show an error
 
-        // If success - update state; save jwt; redirect to /feature
-
-        // If error - show an error
+            });
 
     };
 }
